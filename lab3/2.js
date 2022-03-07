34c4b148416cd5c31ab99855207a935b10b78ec5
Andrew Titov <and020702@gmail.com>
2022-03-07 22:13:29 +0300

    finished lab3

--- lab3/2.js
+++ lab3/2.js
@@ -8,7 +8,22 @@
  * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
  */
 function getMinMax(str) {
-    // code here
+    const result = {
+        min: null,
+        max: null
+    }
+
+    str.replace(',', '')
+    .replace(']', '')
+    .split(' ')
+    .map(number => Number(number))
+    .filter(number => !Number.isNaN(number))
+    .forEach(number => {
+        if (result.min > number || !result.min) result.min = number
+        if (result.max < number || !result.max) result.max = number  
+    })
+
+    return result
 }
 
 module.exports = getMinMax;
