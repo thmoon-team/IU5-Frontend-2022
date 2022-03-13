05df0d1030d474004aa2dd2a78c40297d8af9ef0
trebukov-daniel <trebukovD@yandex.ru>
2022-03-13 23:40:00 +0300

    лаб3

--- lab3/2.js
+++ lab3/2.js
@@ -8,7 +8,21 @@
  * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
  */
 function getMinMax(str) {
-    // code here
+    let min;
+    let max;
+    str = str.split(/[\ :!&?;,\]]/);
+    min = 1000000000;
+    max = -1000000000;
+
+    for(let i = 1; i < str.length; i++){
+        str[i] = Number(str[i]);
+        if((str[i] > max) && (typeof str[i] == "number")) 
+            max = str[i];
+        if((str[i] < min) && (typeof str[i] == "number")) 
+            min = str[i];
+    }
+    return {'min': min, 'max': max};
+
 }
 
 module.exports = getMinMax;
